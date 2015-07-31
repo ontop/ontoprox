603c5e633425a254d22895591e849421c33e8c58
Guohui Xiao <gh.xiao@gmail.com>
2015-07-31 17:07:13 +0200

    finish merge

--- src/main/java/org/semanticweb/ontop/beyondql/approximation/QualifiedExistentialNormalizer.java
+++ src/main/java/org/semanticweb/ontop/beyondql/approximation/QualifiedExistentialNormalizer.java
@@ -1,4 +1,4 @@
-package org.semanticweb.ontop.beyondql.approximation;
+package inf.unibz.it.dllite.aproximation.semantic;
 
 import java.util.*;
 
@@ -28,187 +28,179 @@ import org.slf4j.LoggerFactory;
  * right-hand side of concept inclusions with appropriate Dl-LiteR axioms. This
  * step is implemented by method "normalizeQualifiedExistentialRestrictions"
  * </ul>
- * 
+ * <p/>
  * The constructor for this class requires an OWLOntologyManager to create and
  * manipulate ontologies.
- * 
- * @author Elena Botoeva
  *
+ * @author Elena Botoeva
  *****************************************************************************/
-public class QualifiedExistentialNormalizer extends OntologyTransformer {
+public class QualifiedExistentialNormalizer extends OntologyTransformations {
 
-	/**
-	 * 
-	 * @param manager
-	 */
-	public QualifiedExistentialNormalizer(OWLOntologyManager manager) {
-		super(manager);
-	}
+    /**
+     * @param manager
+     */
+    public QualifiedExistentialNormalizer(OWLOntologyManager manager) {
+        super(manager);
+    }
 
-	// For the logging
-	private Logger log = LoggerFactory.getLogger(QualifiedExistentialNormalizer.class);
-	private final String roleFillerSeparator = "__";
-
-
-	/**
-	 * The normalization that gets rid of qualifiend existential restrictions on
-	 * the right-hand side of concept inclusions by introducing fresh role names
-	 * for each concept of the from ER.(A1 \AND ... \AND An).
-	 * 
-	 * We assume that ontology is in Horn-ALCHIG normal form. Therefore, it is
-	 * enough to consider only SubClassOf axioms (no EquivalentClasses, no
-	 * Domain, nor Range axioms).
-	 * 
-	 * @param ontology
-	 *            in Horn-ALCHIQ normal form
-	 * @param outputIRI
-	 *            the IRI of the new ontology
-	 * 
-	 * @return a version of ontology without qualified existentials on the RHS
-	 * 
-	 * @throws OWLOntologyCreationException
-	 */
-	public OWLOntology transform(
-			OWLOntology ontology, IRI outputIRI)
-			throws OWLOntologyCreationException {
-		log.info("Normalizing qualified existential restrictions on the RHS of concept inclusions");
+    // For the logging
+    private Logger log = LoggerFactory.getLogger(QualifiedExistentialNormalizer.class);
+    private final String roleFillerSeparator = "__";
 
 
-		/**
-		 * Go through the set of axioms in ontology, and either substitute the
-		 * axiom with the new axioms without qualified existential on the RHS,
-		 * or keep the original axiom.
-		 */
-		Set<OWLAxiom> axioms = new HashSet<>();
-		for (OWLAxiom axiom : ontology.getAxioms()) {
-			if (requiresSubstitutionOfQualifiedExistentialRestrictionOnRHS(axiom)) {
-				axioms
-						.addAll(substituteQualifiedExistentialRestrictionOnRHS((OWLSubClassOfAxiom) axiom));
-			} else {
-				axioms.add(axiom);
-			}
-		}
+    /**
+     * The normalization that gets rid of qualifiend existential restrictions on
+     * the right-hand side of concept inclusions by introducing fresh role names
+     * for each concept of the from ER.(A1 \AND ... \AND An).
+     * <p/>
+     * We assume that ontology is in Horn-ALCHIG normal form. Therefore, it is
+     * enough to consider only SubClassOf axioms (no EquivalentClasses, no
+     * Domain, nor Range axioms).
+     *
+     * @param ontology  in Horn-ALCHIQ normal form
+     * @param outputIRI the IRI of the new ontology
+     * @return a version of ontology without qualified existentials on the RHS
+     * @throws OWLOntologyCreationException
+     */
+    public OWLOntology transform(
+            OWLOntology ontology, IRI outputIRI)
+            throws OWLOntologyCreationException {
+        log.info("Normalizing qualified existential restrictions on the RHS of concept inclusions");
 
-		/**
-		 * Create the output ontology from the set of axioms
-		 */
-		OWLOntology output_ont = ontologyManager.createOntology(axioms, outputIRI);
-		log.info("Created output ontology : "
-				+ output_ont.getOntologyID().getOntologyIRI() + " with " +
-				output_ont.getAxiomCount() + " axioms");
 
-		return output_ont;
-	}
+        /**
+         * Go through the set of axioms in ontology, and either substitute the
+         * axiom with the new axioms without qualified existential on the RHS,
+         * or keep the original axiom.
+         */
+        Set<OWLAxiom> axioms = new HashSet<>();
+        for (OWLAxiom axiom : ontology.getAxioms()) {
+            if (requiresSubstitutionOfQualifiedExistentialRestrictionOnRHS(axiom)) {
+                axioms.addAll(substituteQualifiedExistentialRestrictionOnRHS((OWLSubClassOfAxiom) axiom));
+            } else {
+                axioms.add(axiom);
+            }
+        }
 
-	/**
-	 * We do a substitution only if axiom is an #OWLSubClassOfAxiom and the
-	 * super class is a qualified restriction where the property is a direct or
-	 * inverse property, an the filler is a conjunction of atomic concepts
-	 * 
-	 * @param axiom
-	 * @return
-	 */
-	private boolean requiresSubstitutionOfQualifiedExistentialRestrictionOnRHS(
-			OWLAxiom axiom) {
-		boolean requires = false;
+        /**
+         * Create the output ontology from the set of axioms
+         */
+        OWLOntology output_ont = ontologyManager.createOntology(axioms, outputIRI);
+        log.info("Created output ontology : "
+                + output_ont.getOntologyID().getOntologyIRI() + " with " +
+                output_ont.getAxiomCount() + " axioms");
 
-		if (axiom instanceof OWLSubClassOfAxiom) {
-			OWLClassExpression superClass = ((OWLSubClassOfAxiom) axiom)
-					.getSuperClass();
+        return output_ont;
+    }
 
-			if (superClass instanceof OWLObjectSomeValuesFrom) {
-				OWLClassExpression filler = ((OWLObjectSomeValuesFrom) superClass)
-						.getFiller();
-							
-				// Filler must not be Thing
-				if(filler.isOWLThing()){
-					// does nothing
-				}
-				// Filler can be a concept name or
-				else if (filler instanceof OWLClass) {
-					requires = true;
-				}
-				// a conjunction of concept names
-				else if (filler instanceof OWLObjectIntersectionOf) {
-					requires = true;
-					for (OWLClassExpression clazz : ((OWLObjectIntersectionOf) filler)
-							.getOperands()) {
-						if (!(clazz instanceof OWLClass)) {
-							requires = false;
-						}
-					}
-				}
-			}
-		}
+    /**
+     * We do a substitution only if axiom is an #OWLSubClassOfAxiom and the
+     * super class is a qualified restriction where the property is a direct or
+     * inverse property, an the filler is a conjunction of atomic concepts
+     *
+     * @param axiom
+     * @return
+     */
+    private boolean requiresSubstitutionOfQualifiedExistentialRestrictionOnRHS(
+            OWLAxiom axiom) {
+        boolean requires = false;
 
-		return requires;
-	}
+        if (axiom instanceof OWLSubClassOfAxiom) {
+            OWLClassExpression superClass = ((OWLSubClassOfAxiom) axiom)
+                    .getSuperClass();
 
-	/**
-	 * It is assumed that the axiom was checked and it requires substitution.
-	 * Therefore, we assume that axiom is of the form
-	 * <pre> 
-	 * 		C \ISA ∃R.(A1 \AND ... \AND An)
-	 * </pre>
-	 * We introduce a fresh role name P and return the axioms:
-	 * <pre>
-	 * 		C \ISA ∃P.Top 
-	 * 		P \ISA R 
-	 * 		∃P^- \ISA A1 
-	 * 			... 
-	 * 		∃P^- \ISA An
-	 * </pre>
-	 * @param axiom
-	 * @return
-	 */
-	private Set<OWLAxiom> substituteQualifiedExistentialRestrictionOnRHS(
-			OWLSubClassOfAxiom axiom) {
-		Set<OWLAxiom> axioms = new HashSet<>();
+            if (superClass instanceof OWLObjectSomeValuesFrom) {
+                OWLClassExpression filler = ((OWLObjectSomeValuesFrom) superClass)
+                        .getFiller();
 
-		OWLClassExpression superClass = ((OWLSubClassOfAxiom) axiom)
-				.getSuperClass();
-		OWLClassExpression subClass = ((OWLSubClassOfAxiom) axiom)
-				.getSubClass();
+                // Filler must not be Thing
+                if (filler.isOWLThing()) {
+                    // does nothing
+                }
+                // Filler can be a concept name or
+                else if (filler instanceof OWLClass) {
+                    requires = true;
+                }
+                // a conjunction of concept names
+                else if (filler instanceof OWLObjectIntersectionOf) {
+                    requires = true;
+                    for (OWLClassExpression clazz : ((OWLObjectIntersectionOf) filler)
+                            .getOperands()) {
+                        if (!(clazz instanceof OWLClass)) {
+                            requires = false;
+                        }
+                    }
+                }
+            }
+        }
 
-		OWLObjectPropertyExpression prop = ((OWLObjectSomeValuesFrom) superClass)
-				.getProperty();
-		OWLClassExpression filler = ((OWLObjectSomeValuesFrom) superClass)
-				.getFiller();
+        return requires;
+    }
 
-		
-		// Create a fresh role name
-		IRI new_role_iri = IRI.create(IRIUtils.extractPrefix(prop)  
-				+ IRIUtils.extractPredicateName(prop) + roleFillerSeparator 
-				+ IRIUtils.extractConceptNames(filler));
-		OWLObjectProperty new_role = ontologyManager.getOWLDataFactory()
-				.getOWLObjectProperty(new_role_iri);
+    /**
+     * It is assumed that the axiom was checked and it requires substitution.
+     * Therefore, we assume that axiom is of the form
+     * <pre>
+     * 		C \ISA ∃R.(A1 \AND ... \AND An)
+     * </pre>
+     * We introduce a fresh role name P and return the axioms:
+     * <pre>
+     * 		C \ISA ∃P.Top
+     * 		P \ISA R
+     * 		∃P^- \ISA A1
+     * 			...
+     * 		∃P^- \ISA An
+     * </pre>
+     *
+     * @param axiom
+     * @return
+     */
+    private Set<OWLAxiom> substituteQualifiedExistentialRestrictionOnRHS(
+            OWLSubClassOfAxiom axiom) {
+        Set<OWLAxiom> axioms = new HashSet<>();
 
-		// add the axiom C \ISA ∃P.Top
-		axioms.add(ontologyManager.getOWLDataFactory().getOWLSubClassOfAxiom(
-				subClass,
-				ontologyManager.getOWLDataFactory().getOWLObjectSomeValuesFrom(
-						new_role,
-						ontologyManager.getOWLDataFactory().getOWLThing())));
+        OWLClassExpression superClass = axiom.getSuperClass();
+        OWLClassExpression subClass = axiom.getSubClass();
 
-		// add the axiom P \ISA R
-		axioms.add(ontologyManager.getOWLDataFactory()
-				.getOWLSubObjectPropertyOfAxiom(new_role, prop));
+        OWLObjectPropertyExpression prop = ((OWLObjectSomeValuesFrom) superClass)
+                .getProperty();
+        OWLClassExpression filler = ((OWLObjectSomeValuesFrom) superClass)
+                .getFiller();
 
-		// add the axioms ∃P^- \ISA Ai
-		if (filler instanceof OWLClass) {
-			axioms.add(ontologyManager.getOWLDataFactory()
-					.getOWLObjectPropertyRangeAxiom(new_role, filler));
-		} else {
-			for (OWLClassExpression clazz : ((OWLObjectIntersectionOf) filler)
-					.getOperands()) {
-				OWLObjectPropertyRangeAxiom rangeAxiom = ontologyManager
-						.getOWLDataFactory().getOWLObjectPropertyRangeAxiom(
-								new_role, clazz);
-				axioms.add(rangeAxiom);
-			}
-		}
 
-		return axioms;
-	}
+        // Create a fresh role name
+        IRI new_role_iri = IRI.create(extractPrefix(prop)
+                + extractPredicateName(prop) + roleFillerSeparator
+                + extractConceptNames(filler));
+        OWLObjectProperty new_role = ontologyManager.getOWLDataFactory()
+                .getOWLObjectProperty(new_role_iri);
+
+        // add the axiom C \ISA ∃P.Top
+        axioms.add(ontologyManager.getOWLDataFactory().getOWLSubClassOfAxiom(
+                subClass,
+                ontologyManager.getOWLDataFactory().getOWLObjectSomeValuesFrom(
+                        new_role,
+                        ontologyManager.getOWLDataFactory().getOWLThing())));
+
+        // add the axiom P \ISA R
+        axioms.add(ontologyManager.getOWLDataFactory()
+                .getOWLSubObjectPropertyOfAxiom(new_role, prop));
+
+        // add the axioms ∃P^- \ISA Ai
+        if (filler instanceof OWLClass) {
+            axioms.add(ontologyManager.getOWLDataFactory()
+                    .getOWLObjectPropertyRangeAxiom(new_role, filler));
+        } else {
+            for (OWLClassExpression clazz : ((OWLObjectIntersectionOf) filler)
+                    .getOperands()) {
+                OWLObjectPropertyRangeAxiom rangeAxiom = ontologyManager
+                        .getOWLDataFactory().getOWLObjectPropertyRangeAxiom(
+                                new_role, clazz);
+                axioms.add(rangeAxiom);
+            }
+        }
+
+        return axioms;
+    }
 
 }
